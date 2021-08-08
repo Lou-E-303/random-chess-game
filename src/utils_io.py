0be7d9ec7ee8e38aac16dcd724d930722318bbff
Lou-E-303 <tyrone.cowell@tesco.com>
2021-08-08 11:30:27 +0100

    Added ability to write to PGN file

--- src/utils_io.py
+++ src/utils_io.py
@@ -1,3 +1,6 @@
+import os
+
+
 def print_board(board_to_print):
     print("\n")
     print(board_to_print)
@@ -7,3 +10,7 @@ def print_board(board_to_print):
 def print_game(game_to_print, board):
     game_to_print.headers["Result"] = board.result()
     print(game_to_print)
+
+
+def save_pgn_to_file(game_to_save, filepath):
+    print(game_to_save, file=open(filepath, "w"), end="\n\n")

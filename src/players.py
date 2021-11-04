4448c47425434e7c58cd14dc04a1cbd3871c8d8d
Lou-E-303 <louiscowell404@gmail.com>
2021-11-04 22:36:06 +0000

    Get user input in algebraic format instead of UCI

--- src/players.py
+++ src/players.py
@@ -12,15 +12,14 @@ class HumanPlayer:
     def make_move(self, board):
         legal_moves = list(board.legal_moves)
         while True:
-            move = self.get_human_move_input(legal_moves)
+            move = self.get_human_move_input(legal_moves, board)
             if utils_game.check_move_is_valid(board, move):
-                print("DEBUG: Move valid. Returning move: ", move)
                 break
             else:
                 print("Move: " + chess.move.uci(move) + " is invalid. Please try again.")
         return move
 
-    def get_human_move_input(self, legal_moves):
+    def get_human_move_input(self, legal_moves, board):
         print("Legal Moves: ", legal_moves)
-        selected_move = input("Please enter a move using UCI notation: ") # TODO change to algebraic
-        return chess.Move.from_uci(selected_move)
+        selected_move = board.parse_san(input("Please enter a move in algebraic notation: "))
+        return selected_move
